3d570d72eef625c93eb770911be89970b7c7f603
Karol Szpakowski <karol.szpakowski@gmail.com>
2024-07-25 23:41:00 +0200

    fix printing response

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const url = process.env.FETCH_URL
 const interval = process.env.FETCH_INTERVAL_MS
 setInterval(() =>
     fetch(url)
-        .then(res => console.log(JSON.stringify(res)))
+        .then(res => res.text())
+        .then(console.log)
         .catch(e => console.error(e))
     , interval)
